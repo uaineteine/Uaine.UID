da480372c604e0dc9f66e187c36fdf6fe400b44a
Daniel Robert <Daniel.Robert.S-S@hotmail.com>
2021-07-26 15:52:47 +1000

    added user class

---
+++ Uaine.UID/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Uaine.Objects.Primitives;
+
+namespace Uaine.UID
+{
+    public class User : NamedObject
+    {
+        public UniqueID ID;
+        public User(string name, bool posUniqueIDOnly) : base(name)
+        {
+            Random rand = new Random();
+            ID = new UniqueID(ref rand, posUniqueIDOnly);
+        }
+    }
+}
